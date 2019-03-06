1015a0a9211ea7e37e5c4f1099e54db1ec867215
Mathias Johansson <mathias9807@gmail.com>
2019-03-06 09:39:25 +0100

    Add project structure for frontend and replica managers

---
+++ DistributedCAD/src/replicaManager/ReplicaManager.java
@@ -0,0 +1,14 @@
+package replicaManager;
+
+public class ReplicaManager {
+
+	public ReplicaManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
