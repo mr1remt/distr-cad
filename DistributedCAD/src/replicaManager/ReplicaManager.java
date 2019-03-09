1b24a881c3c03bd1b1e8b11915c1685c0d0a73b5
Mathias Johansson <mathias9807@gmail.com>
2019-03-09 17:27:01 +0100

    Initial ReplicaManager implementation

--- DistributedCAD/src/replicaManager/ReplicaManager.java
+++ DistributedCAD/src/replicaManager/ReplicaManager.java
@@ -1,14 +1,76 @@
 package replicaManager;
 
-public class ReplicaManager {
+import java.io.InputStream;
+import java.io.OutputStream;
 
-	public ReplicaManager() {
-		// TODO Auto-generated constructor stub
+import org.jgroups.Address;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.Receiver;
+import org.jgroups.View;
+
+public class ReplicaManager implements Receiver {
+	
+	private JChannel channel;
+
+	public ReplicaManager() throws Exception {
+		channel = new JChannel();
+		channel.setReceiver(this);
+		channel.connect("CAD-Service");
+		
+		while (true) {
+			Thread.sleep(1000000000);
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			new ReplicaManager();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
+	@Override
+	public void getState(OutputStream os) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receive(Message m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setState(InputStream is) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void block() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suspect(Address addr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unblock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewAccepted(View view) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
